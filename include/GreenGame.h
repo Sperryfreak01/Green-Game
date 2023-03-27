e534aa691d8461a75fa0633d82d8c9cd50c2d77d
Lovett <Matt.Lovett@spectrumbrands.com>
2023-03-26 23:22:35 -0700

    moved a nunch of stuff and hid the secrets

---
+++ include/GreenGame.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+//#define HOST "http://green.mattlovett.com/esp32fw.bin"
+
+#define  redLEDs 0
+#define  blueLEDs 1
+#define  greenLEDs 2
+#define  whiteLEDs 3
+#define Logging "remote"
